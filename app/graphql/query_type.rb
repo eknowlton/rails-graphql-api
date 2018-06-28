7082b19d3d5d07c4e39e270a3f59143cc4b9ad3c
Josh Forbes <jdforbes82@gmail.com>
2018-06-28 15:53:49 -0400

    Add the ‘me’ query to expose the current user (#19)

--- app/graphql/query_type.rb
+++ app/graphql/query_type.rb
@@ -1,4 +1,5 @@
 class QueryType < Types::BaseObject
+  field :me, resolver: MeQuery
   field :users, resolver: UsersQuery
   field :user, resolver: UserQuery
   field :roles, resolver: RolesQuery
---
+++ spec/operations/me_query_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Me Query API', :graphql do
+  describe 'me' do
+    let(:query) do
+      <<~'GRAPHQL'
+        query {
+          me {
+            id
+          }
+        }
+      GRAPHQL
+    end
+
+    it 'gets the current user' do
+      user = create(:user)
+
+      result = execute query, as: user
+
+      id = result[:data][:me][:id]
+      expect(id).to eq(user.id.to_s)
+    end
+  end
+end
