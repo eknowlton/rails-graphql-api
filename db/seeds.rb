8c69bacac59dfaf10bdd97defdf7ce6dbc0c99e3
Josh Forbes <jdforbes82@gmail.com>
2019-03-11 10:26:09 -0400

    Add new users (#64)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
 
-role = Role.create!(
+user_data = [
+  {
+    first_name: "John",
+    last_name: "Doe",
+    abbreviation: "JAD",
+    email: "test@kimmel.com",
+  },
+  {
+    first_name: "Jane",
+    last_name: "Doe",
+    abbreviation: "JBD",
+    email: "example@kimmel.com",
+  },
+  {
+    first_name: "Jacob",
+    last_name: "Haug",
+    email: "jacob@kimmel.com",
+    abbreviation: "JEH",
+  },
+  {
+    first_name: "Alex",
+    last_name: "Smith",
+    email: "demo@kimmel.com",
+    abbreviation: "AXS",
+  },
+  {
+    first_name: "Mary",
+    last_name: "Jones",
+    email: "software@kimmel.com",
+    abbreviation: "MRJ",
+  },
+  {
+    first_name: "Richard",
+    last_name: "Roe",
+    email: "development@kimmel.com",
+    abbreviation: "RRR",
+  },
+]
+
+admin_role = Role.create!(
   name: "Admin"
 )
+SyncAbilities.new(admin_role, Abilities).call
 
-SyncAbilities.new(role, Abilities).call
-
-User.create!(
-  walter_id: 1,
-  intranet_id: 1,
-  role: role,
-  email: "test@kimmel.com",
-  password: "Tester12",
-  first_name: "John",
-  last_name: "Doe",
-  abbreviation: "JAD",
-  title: "Software Developer",
-  hire_date: "2015/01/01"
-)
-
-User.create!(
-  walter_id: 2,
-  intranet_id: 2,
-  role: role,
-  email: "example@kimmel.com",
-  password: "Tester12",
-  first_name: "Jane",
-  last_name: "Doe",
-  abbreviation: "JBD",
-  title: "Quality Assurance",
-  hire_date: "2015/01/01"
-)
+user_data.each do |data|
+  User.create!(
+    role: admin_role,
+    email: data[:email],
+    password: "Tester12",
+    first_name: data[:first_name],
+    last_name: data[:last_name],
+    abbreviation: data[:abbreviation],
+    title: Faker::Job.title,
+    hire_date: Faker::Date.between(10.years.ago, Date.current)
+  )
+end
