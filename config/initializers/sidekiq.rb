716601def305bafcff6aae82db75b28aad5d1de8
Ethan Knowlton <eknowlton@gmail.com>
2019-05-23 15:07:09 -0400

    No way for server to decide which redis host (#70)
    * # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    Update kafka staging keys
    
    # This is the commit message #2:
    
    No way for server to decide which redis host
    
    In order for the server to decide which redist host to use i have moved
    the redis url to be stored into a environment variable.
    
    I've also updated some `ENV`'s to `ENV.fetch` to be more consistent.
    Here is a interesting article to explain the difference.
    
    https://stackoverflow.com/questions/47985660/rails-difference-between-env-fetch-and-env
    
    * fixed standardrb formatting error for lint

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: Rails.application.credentials.dig(Rails.env.to_sym, :redis_url),
+    url: ENV.fetch("REDIS_URL") { "localhost:6379" },
     namespace: "sidekiq_central_#{Rails.env}",
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: Rails.application.credentials.dig(Rails.env.to_sym, :redis_url),
+    url: ENV.fetch("REDIS_URL") { "localhost:6379" },
     namespace: "sidekiq_central_#{Rails.env}",
   }
 end
