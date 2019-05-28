b1645fe16a2ca15679649cb730bfd020d2eaee7e
Josh Forbes <jdforbes82@gmail.com>
2019-05-28 08:47:39 -0400

    Update frontend host
    To reflect move to digital ocean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.frontend_host = "kimmel.chat"
+  config.frontend_host = "sms.kimmel.io"
 end
