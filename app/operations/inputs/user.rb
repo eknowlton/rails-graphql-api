41afab8ccd8b53d23bcc1defc10f6cb9fbafee92
Henry Kiehna <33327590+jhkiehna@users.noreply.github.com>
2019-07-29 14:10:28 -0400

    added walter_id field to user input and added fields other than first_name and last_name to patch_user input (#80)

--- app/operations/inputs/user.rb
+++ app/operations/inputs/user.rb
@@ -2,6 +2,7 @@ module Inputs
   class User < Types::BaseInputObject
     graphql_name "UserInput"
     description "Properties for a User"
+    argument :walter_id, Int, required: false
     argument :email, String, required: true
     argument :first_name, String, required: true
     argument :last_name, String, required: true
